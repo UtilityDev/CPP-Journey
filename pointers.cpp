07f3406bc2b61b3a72f770f1a87435b4d35c7c09
Utility <45518053+UtilityDev@users.noreply.github.com>
2022-02-12 14:14:43 +0100

    First file! POINTERS!

---
+++ pointers.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+// Program entry point
+int main() {
+  int foo = 5;        // Variable 'foo' with a value of 5
+  int* ptr = &foo;    // Pointer variable holding the memory address of the 'foo' variable
+}
